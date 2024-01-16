e6c7e1d59e4d66bda947fd6e9f0ba1f422264b01
Josh LaSota <joshlasota@Joshs-MacBook-Air-2.local>
2024-01-16 11:32:03 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! My name is Josh.");
+        System.out.println("I am a musician and programmer!");
+        System.out.println("This is my second semester at MCC.");
+        System.out.println("I am learning more and more about programming.");
+        System.out.println("Have a great day!");
+    }
+}
